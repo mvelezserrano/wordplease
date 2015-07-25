ff456ba2ac08868670a941dfcddcf82e634cd34f
Miguel Ángel Vélez <mvelezserrano@outlook.es>
2015-07-25 18:51:36 +0200

    Modifico api listado de posts de un blog para mostrar error 404 si ese blog no existe

--- blogs/api.py
+++ blogs/api.py
@@ -2,8 +2,11 @@
 from blogs.models import Post
 from blogs.serializers import BlogSerializer, PostSerializer
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from users.serializers import UserSerializer
 
 
 class BlogListAPI(APIView):
@@ -11,12 +14,13 @@ class BlogListAPI(APIView):
     def get(self, request):
         blogs = User.objects.all()
         serializer = BlogSerializer(blogs, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class PostListAPI(APIView):
 
     def get(self, request, user):
-        posts = Post.objects.filter(owner__username=user).order_by('-pub_date')
+        existing_user = get_object_or_404(User, username=user)
+        posts = Post.objects.filter(owner__username=existing_user.username).order_by('-pub_date')
         serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
